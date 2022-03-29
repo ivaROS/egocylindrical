983436e92c748b3ce0c42dec47b5df5c79ffdc3a
Justin Smith <jssmith@gatech.edu>
2022-03-29 19:43:51 -0400

    Fix hybrid-frame depth image insertion

--- include/egocylindrical/depth_image_inserter.h
+++ include/egocylindrical/depth_image_inserter.h
@@ -16,52 +16,6 @@ namespace egocylindrical
 {
     namespace utils
     {
-      
-//         template <typename T>
-//         struct PointRef
-//         {
-//           T& x, y, z;
-//           
-//           PointRef(T& x, T& y, T& z):
-//             x(x),
-//             y(y),
-//             z(z)
-//             {}
-//         };
-// 
-//         template <typename T>
-//         class PointSource
-//         {
-//         public:
-//             
-//             struct Iterator 
-//             {
-//                 using iterator_category = std::forward_iterator_tag;
-//                 using difference_type   = std::ptrdiff_t;
-//                 using value_type        = PointRef<T>;
-//                 using pointer           = PointRef<T>*;  // or also value_type*
-//                 using reference         = PointRef<T>&;  // or also value_type&
-//                 
-//                 Iterator(PointSource* source):
-//                     source_(source)
-//                 {
-//                   
-//                 }
-//                 
-//             private:
-//                 PointSource* source_;
-//             };
-//             
-//             virtual Iterator::begin()=0;
-//             virtual Iterator::end()=0;
-//         };
-
-
-//         class DepthImagePointSource : public PointSource
-//         {
-//             
-// 
-//         };
 
         template <typename T>
         void insertPoints(utils::ECWrapper& cylindrical_points, const cv::Mat image, const image_geometry::PinholeCameraModel& cam_model, const geometry_msgs::TransformStamped transform)
@@ -89,24 +43,24 @@ namespace egocylindrical
                     
                     T depth = image.at<T>(i,j);
                     
-                    if(depth==depth)  //Only insert actual points
-                    {
                     
+                    if(depth>0)  //Only insert actual points (works for both float and uint16)
+                    {                        
                         cv::Point3f ray = cam_model.projectPixelTo3dRay(pt);
-                        cv::Point3f world_pnt = ray * (float)(depth/scale);
+                        cv::Point3f world_pnt = ray * (((float) depth)/scale);
                         cv::Point3f transformed_pnt = point_transformer.transform(world_pnt);
                         
                         int cyl_idx = cylindrical_points.worldToCylindricalIdx(transformed_pnt);
                         
                         if(cyl_idx >= 0  && cyl_idx < max_ind)
                         {
-                            float depth = worldToRangeSquared(transformed_pnt);
+                            float range_sq = worldToRangeSquared(transformed_pnt);
                     
                             cv::Point3f prev_point(x[cyl_idx], y[cyl_idx], z[cyl_idx]);
                             
-                            float prev_depth = worldToRangeSquared(prev_point);
+                            float prev_range_sq = worldToRangeSquared(prev_point);
                             
-                            if(!(prev_depth <= depth)) //overwrite || 
+                            if(!(prev_range_sq <= range_sq)) //overwrite || 
                             {   
                                 x[cyl_idx] = transformed_pnt.x;
                                 y[cyl_idx] = transformed_pnt.y;
@@ -119,13 +73,13 @@ namespace egocylindrical
                         
                             if(cyl_idx >=0 && cyl_idx < cylindrical_points.getNumPts())
                             {
-                                float depth = worldToCanDepth(transformed_pnt);
+                                float can_depth = worldToCanDepth(transformed_pnt);
                                 
                                 cv::Point3f prev_point(x[cyl_idx], y[cyl_idx], z[cyl_idx]);
                                 
-                                float prev_depth = worldToCanDepth(prev_point);
+                                float prev_can_depth = worldToCanDepth(prev_point);
                                 
-                                if(!(prev_depth <= depth)) //overwrite || 
+                                if(!(prev_can_depth <= can_depth)) //overwrite || 
                                 {   
                                     
                                     x[cyl_idx] = transformed_pnt.x;
@@ -183,6 +137,17 @@ namespace egocylindrical
             
             bool insert(ECWrapper& cylindrical_points, const sensor_msgs::Image::ConstPtr& image_msg, const sensor_msgs::CameraInfo::ConstPtr& cam_info)
             {
+                if( cam_model_.fromCameraInfo(cam_info) )
+                {
+                    ROS_DEBUG("Camera info has changed!");
+                    //If camera info changed, update any precomputed values
+                    //cam_model_.init();
+                }
+                else
+                {
+                    ROS_DEBUG("Camera info has not changed");
+                }
+                
                 //Get transform
                 geometry_msgs::TransformStamped transform;
                 try
@@ -199,6 +164,8 @@ namespace egocylindrical
                 }
                 
                 insertPoints(cylindrical_points, image_msg, cam_model_, transform);
+
+                return true;
             }
 
 
