ca7cd6f6a477af288caffd5f42fe4bf6a126b240
Justin Smith <jssmith@gatech.edu>
2022-03-30 11:50:23 -0400

    Add vectorized depth image insertion implementations

--- include/egocylindrical/depth_image_common.h
+++ include/egocylindrical/depth_image_common.h
@@ -26,8 +26,38 @@ namespace egocylindrical
         };
         
         
+        class InlineCameraModel : public image_geometry::PinholeCameraModel
+        {
+        public:
+          cv::Point2d project3dToPixel(const cv::Point3d& xyz) const
+          {
+            assert( initialized() );
+            assert(P_(2, 3) == 0.0); // Calibrated stereo cameras should be in the same plane
+
+            // [U V W]^T = P * [X Y Z 1]^T
+            // u = U/W
+            // v = V/W
+            cv::Point2d uv_rect;
+            uv_rect.x = (fx()*xyz.x + Tx()) / xyz.z + cx();
+            uv_rect.y = (fy()*xyz.y + Ty()) / xyz.z + cy();
+            return uv_rect;
+          }
+
+          cv::Point3d projectPixelTo3dRay(const cv::Point2d& uv_rect) const
+          {
+            assert( initialized() );
+
+            cv::Point3d ray;
+            ray.x = (uv_rect.x - cx() - Tx()) / fx();
+            ray.y = (uv_rect.y - cy() - Ty()) / fy();
+            ray.z = 1.0;
+            return ray;
+          }
+
+        };
+        
         //Inherits from PinholeCamerModel in order to access protected member function initRectificationMaps
-        class CleanCameraModel : public image_geometry::PinholeCameraModel
+        class CleanCameraModel : public InlineCameraModel
         {
         public:
             void init()
--- src/depth_image_inserter.cpp
+++ src/depth_image_inserter.cpp
@@ -12,14 +12,339 @@
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
 
+#include <cstdlib>
 
 namespace egocylindrical
 {
     namespace utils
     {
-
+        //additional precomputation
         template <typename T>
-        void insertPoints(utils::ECWrapper& cylindrical_points, const cv::Mat image, const image_geometry::PinholeCameraModel& cam_model, const geometry_msgs::TransformStamped transform)
+        void insertPoints4(utils::ECWrapper& cylindrical_points, const cv::Mat image, const CleanCameraModel& cam_model, const geometry_msgs::TransformStamped transform)
+        {
+            cv::Size image_size = cam_model.reducedResolution();
+            const int image_width = image_size.width;
+            const int image_height = image_size.height;
+            const int num_pixels = image_width * image_height;
+            
+            const int max_ind = cylindrical_points.getCols();
+            float* x = cylindrical_points.getX();
+            float* y = cylindrical_points.getY();
+            float* z = cylindrical_points.getZ();
+            
+            PointTransformerObject point_transformer(transform);
+            
+            const uint scale = DepthScale<T>::scale();
+            
+            AlignedVector<float> ranges(num_pixels, dNaN), nx(num_pixels, dNaN), ny(num_pixels, dNaN), nz(num_pixels, dNaN);
+            AlignedVector<uint32_t> inds(num_pixels);
+            
+            const T* const imgptr = (T* const) image.data;
+            
+            const float row_factor = ((float) 1)/image_width;
+            
+            for(int i = 0; i < num_pixels; ++i)
+            {
+                float raw_row = i*row_factor;
+                float row = std::floor(raw_row);
+                float decimal = raw_row - row;
+                float col = decimal * image_width;
+                //auto res = std::div(i, image_width); int row = res.quot; int col = res.rem;
+                //int row = i / image_width;
+                //int col = i % image_width;
+
+                
+                T depth = imgptr[i];
+                
+                cv::Point2d pt(col, row);
+
+                //if(depth>0)  //Only insert actual points (works for both float and uint16)
+                {                        
+                    cv::Point3f ray = cam_model.projectPixelTo3dRay(pt);
+                    cv::Point3f world_pnt = ray * (((float) depth)/scale);
+                    cv::Point3f transformed_pnt = point_transformer.transform(world_pnt);
+                    
+                    nx[i] = transformed_pnt.x;
+                    ny[i] = transformed_pnt.y;
+                    nz[i] = transformed_pnt.z;
+                    
+                    int cyl_idx = cylindrical_points.worldToCylindricalIdx(transformed_pnt);
+                    
+                    float range_sq = worldToRangeSquared(transformed_pnt);
+                    inds[i] = cyl_idx;
+                    ranges[i] = range_sq;
+                }                
+            }
+            
+            for(int i = 0; i < num_pixels; ++i)
+            {
+                if(ranges[i]>0)
+                {
+                    cv::Point3f transformed_pnt(nx[i], ny[i], nz[i]);
+                    
+                    int cyl_idx = inds[i];
+                            
+                    if(cyl_idx >= 0  && cyl_idx < max_ind)
+                    {
+                        float range_sq = ranges[i];
+                
+                        cv::Point3f prev_point(x[cyl_idx], y[cyl_idx], z[cyl_idx]);
+                        
+                        float prev_range_sq = worldToRangeSquared(prev_point);
+                        
+                        if(!(prev_range_sq <= range_sq)) //overwrite || 
+                        {   
+                            x[cyl_idx] = transformed_pnt.x;
+                            y[cyl_idx] = transformed_pnt.y;
+                            z[cyl_idx] = transformed_pnt.z;
+                        }
+                    }
+                    else
+                    {
+                        cyl_idx = cylindrical_points.worldToCanIdx(transformed_pnt);
+                    
+                        if(cyl_idx >=0 && cyl_idx < cylindrical_points.getNumPts())
+                        {
+                            float can_depth = worldToCanDepth(transformed_pnt);
+                            
+                            cv::Point3f prev_point(x[cyl_idx], y[cyl_idx], z[cyl_idx]);
+                            
+                            float prev_can_depth = worldToCanDepth(prev_point);
+                            
+                            if(!(prev_can_depth <= can_depth)) //overwrite || 
+                            {   
+                                
+                                x[cyl_idx] = transformed_pnt.x;
+                                y[cyl_idx] = transformed_pnt.y;
+                                z[cyl_idx] = transformed_pnt.z;
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+      
+        //whole image vectorization
+        template <typename T>
+        void insertPoints3(utils::ECWrapper& cylindrical_points, const cv::Mat image, const CleanCameraModel& cam_model, const geometry_msgs::TransformStamped transform)
+        {
+            cv::Size image_size = cam_model.reducedResolution();
+            const int image_width = image_size.width;
+            const int image_height = image_size.height;
+            const int num_pixels = image_width * image_height;
+            
+            const int max_ind = cylindrical_points.getCols();
+            float* x = cylindrical_points.getX();
+            float* y = cylindrical_points.getY();
+            float* z = cylindrical_points.getZ();
+            
+            PointTransformerObject point_transformer(transform);
+            
+            const uint scale = DepthScale<T>::scale();
+            
+            AlignedVector<float> ranges(num_pixels, dNaN), nx(num_pixels, dNaN), ny(num_pixels, dNaN), nz(num_pixels, dNaN);
+            AlignedVector<uint32_t> inds(num_pixels);
+            
+            const T* const imgptr = (T* const) image.data;
+            
+            const float row_factor = ((float) 1)/image_width;
+            
+            for(int i = 0; i < num_pixels; ++i)
+            {
+                float raw_row = i*row_factor;
+                float row = std::floor(raw_row);
+                float decimal = raw_row - row;
+                float col = decimal * image_width;
+                //auto res = std::div(i, image_width); int row = res.quot; int col = res.rem;
+                //int row = i / image_width;
+                //int col = i % image_width;
+
+                
+                T depth = imgptr[i];
+                
+                cv::Point2d pt(col, row);
+
+                //if(depth>0)  //Only insert actual points (works for both float and uint16)
+                {                        
+                    cv::Point3f ray = cam_model.projectPixelTo3dRay(pt);
+                    cv::Point3f world_pnt = ray * (((float) depth)/scale);
+                    cv::Point3f transformed_pnt = point_transformer.transform(world_pnt);
+                    
+                    nx[i] = transformed_pnt.x;
+                    ny[i] = transformed_pnt.y;
+                    nz[i] = transformed_pnt.z;
+                    
+                    //int cyl_idx = cylindrical_points.worldToCylindricalIdx(transformed_pnt);
+                    
+                    float range_sq = worldToRangeSquared(transformed_pnt);
+                    
+                    ranges[i] = range_sq;
+                }                
+            }
+            
+            for(int i = 0; i < num_pixels; ++i)
+            {
+                if(ranges[i]>0)
+                {
+                    cv::Point3f transformed_pnt(nx[i], ny[i], nz[i]);
+                    
+                    int cyl_idx = cylindrical_points.worldToCylindricalIdx(transformed_pnt);
+                            
+                    if(cyl_idx >= 0  && cyl_idx < max_ind)
+                    {
+                        float range_sq = ranges[i];
+                
+                        cv::Point3f prev_point(x[cyl_idx], y[cyl_idx], z[cyl_idx]);
+                        
+                        float prev_range_sq = worldToRangeSquared(prev_point);
+                        
+                        if(!(prev_range_sq <= range_sq)) //overwrite || 
+                        {   
+                            x[cyl_idx] = transformed_pnt.x;
+                            y[cyl_idx] = transformed_pnt.y;
+                            z[cyl_idx] = transformed_pnt.z;
+                        }
+                    }
+                    else
+                    {
+                        cyl_idx = cylindrical_points.worldToCanIdx(transformed_pnt);
+                    
+                        if(cyl_idx >=0 && cyl_idx < cylindrical_points.getNumPts())
+                        {
+                            float can_depth = worldToCanDepth(transformed_pnt);
+                            
+                            cv::Point3f prev_point(x[cyl_idx], y[cyl_idx], z[cyl_idx]);
+                            
+                            float prev_can_depth = worldToCanDepth(prev_point);
+                            
+                            if(!(prev_can_depth <= can_depth)) //overwrite || 
+                            {   
+                                
+                                x[cyl_idx] = transformed_pnt.x;
+                                y[cyl_idx] = transformed_pnt.y;
+                                z[cyl_idx] = transformed_pnt.z;
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+        
+        //row-wise vectorization
+        template <typename T>
+        void insertPoints2(utils::ECWrapper& cylindrical_points, const cv::Mat image, const CleanCameraModel& cam_model, const geometry_msgs::TransformStamped transform)
+        {
+            cv::Size image_size = cam_model.reducedResolution();
+            const int image_width = image_size.width;
+            const int image_height = image_size.height;
+            const int num_pixels = image_width * image_height;
+            
+            const int max_ind = cylindrical_points.getCols();
+            float* x = cylindrical_points.getX();
+            float* y = cylindrical_points.getY();
+            float* z = cylindrical_points.getZ();
+            
+            PointTransformerObject point_transformer(transform);
+            
+            const uint scale = DepthScale<T>::scale();
+            
+            AlignedVector<float> ranges(num_pixels, dNaN), nx(num_pixels, dNaN), ny(num_pixels, dNaN), nz(num_pixels, dNaN);
+            AlignedVector<uint32_t> inds(num_pixels);
+            
+            const T* const imgptr = (T* const) image.data;
+            
+            //for(int i = 0; i < num_pixels; ++i)
+            //{
+            int i = 0;
+            for(int row = 0; row < image_height; ++row)
+            {
+                for(int col = 0; col < image_width; ++col)
+                {                       
+                    //auto res = std::div(i, image_width); int row = res.quot; int col = res.rem;
+                    //int row = i / image_width;
+                    //int col = i % image_width;
+                    
+                    //int row = i+10;
+                    //int col = i + 30;
+                    
+                    T depth = imgptr[i];
+                    
+                    cv::Point2d pt(col, row);
+
+                    //if(depth>0)  //Only insert actual points (works for both float and uint16)
+                    {                        
+                        cv::Point3f ray = cam_model.projectPixelTo3dRay(pt);
+                        cv::Point3f world_pnt = ray * (((float) depth)/scale);
+                        cv::Point3f transformed_pnt = point_transformer.transform(world_pnt);
+                        
+                        nx[i] = transformed_pnt.x;
+                        ny[i] = transformed_pnt.y;
+                        nz[i] = transformed_pnt.z;
+                        
+                        //int cyl_idx = cylindrical_points.worldToCylindricalIdx(transformed_pnt);
+                        
+                        float range_sq = worldToRangeSquared(transformed_pnt);
+                        
+                        ranges[i] = range_sq;
+                    }
+                    i++;
+                }
+            }
+            
+            for(int i = 0; i < num_pixels; ++i)
+            {
+                if(ranges[i]>0)
+                {
+                    cv::Point3f transformed_pnt(nx[i], ny[i], nz[i]);
+                    
+                    int cyl_idx = cylindrical_points.worldToCylindricalIdx(transformed_pnt);
+                            
+                    if(cyl_idx >= 0  && cyl_idx < max_ind)
+                    {
+                        float range_sq = ranges[i];
+                
+                        cv::Point3f prev_point(x[cyl_idx], y[cyl_idx], z[cyl_idx]);
+                        
+                        float prev_range_sq = worldToRangeSquared(prev_point);
+                        
+                        if(!(prev_range_sq <= range_sq)) //overwrite || 
+                        {   
+                            x[cyl_idx] = transformed_pnt.x;
+                            y[cyl_idx] = transformed_pnt.y;
+                            z[cyl_idx] = transformed_pnt.z;
+                        }
+                    }
+                    else
+                    {
+                        cyl_idx = cylindrical_points.worldToCanIdx(transformed_pnt);
+                    
+                        if(cyl_idx >=0 && cyl_idx < cylindrical_points.getNumPts())
+                        {
+                            float can_depth = worldToCanDepth(transformed_pnt);
+                            
+                            cv::Point3f prev_point(x[cyl_idx], y[cyl_idx], z[cyl_idx]);
+                            
+                            float prev_can_depth = worldToCanDepth(prev_point);
+                            
+                            if(!(prev_can_depth <= can_depth)) //overwrite || 
+                            {   
+                                
+                                x[cyl_idx] = transformed_pnt.x;
+                                y[cyl_idx] = transformed_pnt.y;
+                                z[cyl_idx] = transformed_pnt.z;
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+      
+        //immediate insert
+        template <typename T>
+        void insertPoints(utils::ECWrapper& cylindrical_points, const cv::Mat image, const CleanCameraModel& cam_model, const geometry_msgs::TransformStamped transform)
         {
             cv::Size image_size = cam_model.reducedResolution();
             int image_width = image_size.width;
@@ -98,17 +423,17 @@ namespace egocylindrical
 
 
         inline
-        void insertPoints(utils::ECWrapper& cylindrical_points, const sensor_msgs::Image::ConstPtr& image_msg, const image_geometry::PinholeCameraModel& cam_model, const geometry_msgs::TransformStamped transform)
+        void insertPoints(utils::ECWrapper& cylindrical_points, const sensor_msgs::Image::ConstPtr& image_msg, const CleanCameraModel& cam_model, const geometry_msgs::TransformStamped transform)
         {
             const cv::Mat image = cv_bridge::toCvShare(image_msg)->image;
 
             if(image.depth() == CV_32FC1)
             {
-                insertPoints<float>(cylindrical_points, image, cam_model, transform);
+                insertPoints3<float>(cylindrical_points, image, cam_model, transform);
             }
             else if (image.depth() == CV_16UC1)
             {
-                insertPoints<uint16_t>(cylindrical_points, image, cam_model, transform);
+                insertPoints3<uint16_t>(cylindrical_points, image, cam_model, transform);
             }
         }
 
